3a34df4283bddffbf9edde114f50d4e5ff0ba01e
Pierre Nel <pierre.nel@gmail.com>
2019-03-12 16:42:10 +0200

    device sizes

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
 import { configure } from '@storybook/react';
+import { addParameters } from '@storybook/react';
 
 function loadStories() {
   require('../src/stories');
 }
+
+// addParameters({ viewport: options });
 
 configure(loadStories, module);
